29319b864fff2a9d9f846994d9bd7ee4daa84f9d
AndresFDaza <andresdaza04@hotmail.com>
2020-12-31 23:46:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Screen from './components/Screen'
+
+function App() {
+  return (
+    <div>
+      <h1>thanks 2020</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Screen.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Scren extends React.Component{
+  render(){
+    return(
+      <div>
+        <div>
+          <h1>BikeParking UMNG</h1>
+        </div>
+        <nav>
+          <a>Buscar</a>
+          <a>Registrarse</a>
+          <a>Actualizar</a>
+        </nav>
+        <div>
+          VIEW
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Scren
